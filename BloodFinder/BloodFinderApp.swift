26180443e2c3214ea47da2541c0d1d0801c8a6a1
csemozid <csemozid@gmail.com>
2022-03-16 19:34:04 +0600

    Initial Commit

---
+++ BloodFinder/BloodFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloodFinderApp.swift
+//  BloodFinder
+//
+//  Created by Md. Abdul Mozid on 16/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BloodFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BloodFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BloodFinder
+//
+//  Created by Md. Abdul Mozid on 16/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
